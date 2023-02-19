26e75c0357ca8bbfe966e41377a1725d30be59ca
Sidheshwar Choure <sidheshwarchoure@gmail.com>
2023-02-19 19:37:24 +0530

    change

--- index.js
+++ index.js
@@ -1,46 +1,134 @@
-let issuedBooks = [];
+const bookNameElement = document.getElementById('book-name');
+const issueToElement = document.getElementById("issue-to");
+const btnElement = document.getElementById('btn');
+const tableBody = document.querySelector("tbody");
+const books = [
+];
 
-      function issueBook() {
-        const bookName = document.getElementById("bookName").value;
-        const issuedTo = document.getElementById("issuedto").value;
-        const issuedTime = new Date();
-        const bookId = issuedBooks.length + 1;
-        const bookStatus = "not returned";
+function handleEdit(event) {
+   const buttonElement  = event.target;
+   console.log(buttonElement);
+    const id = buttonElement.id;
 
-        issuedBooks.push({
-          id: bookId,
-          bookname: bookName,
-          issuedto: issuedTo,
-          issuedtime: issuedTime,
-          status: bookStatus,
-        });
+   if (buttonElement.textContent === 'Edit') {
+    event.target.textContent = "Save";
+    const parentElement = buttonElement.parentElement;
 
-        refresh();
-      }
+    parentElement.removeChild(parentElement.firstChild);
 
-      function refresh() {
-        const tableBody = document.getElementById("show");
-        tableBody.innerHTML = "";
+    const input = document.createElement("input");
+    input.id = 'status-inp';
+    input.value = books[id - 1].status;
+    parentElement.insertBefore(input, event.target);
+   } 
+   else{
+      const statusElement = document.getElementById('status-inp');
+      books[id - 1].status = statusElement.value;
+      renderBooksInsideTable();
+   }
 
-        issuedBooks.forEach((book) => {
-          const row = document.createElement("tr");
-          row.innerHTML = `
-					<td>${book.id}</td>
-                    <td>${book.bookname}</td>
-					<td>${book.issuedto}</td>
-					<td>${book.issuedtime}</td>
-					<td class="status {book.status.replace(" ", "-")}">${book.status}</td>
-				`;
-          row.onclick = () => editStatus(book);
-          tableBody.appendChild(row);
-        });
-      }
+}
+function createTableRow (data, tableBody, bookId) {
+    // First create a tr
+    //  const tr = `
+    //   <tr>
+    //     <td> ${index} </td>
+    //     <td> ${data.name} </td>
+    //     <td> ${data.status} </td>
+    //   </tr>
+    //  `;
 
-      function editStatus(book) {
-        if (book.status === "not returned") {
-          book.status = "returned";
-        } else {
-          book.status = "not returned";
-        }
-        refresh();
-      }
+    //  tableBody.innerHtml += tr;
+
+    const tr = document.createElement("tr");
+    // create 5 ths and add data inside it
+
+    const idTd = document.createElement("td");
+    idTd.textContent = bookId;
+
+    const bookNameTd = document.createElement("td");
+    bookNameTd.textContent = data.name;
+
+     const issueToTd = document.createElement("td");
+     issueToTd.textContent = data.issuedTo;
+
+
+    const issuedAtTd = document.createElement("td");
+    issuedAtTd.textContent = data.issuedAt;
+
+    const statusTd = document.createElement("td");
+    statusTd.classList.add('flex');
+
+    const button = document.createElement('button');
+    const span = document.createElement('span');
+
+    span.textContent = data.status;
+    const className = data.status === "not returned" ? "red" : "green";
+    span.classList.add(className);
+
+    button.textContent = 'Edit';
+    button.id = bookId;
+    button.addEventListener("click", handleEdit);
+
+    statusTd.appendChild(span);
+    statusTd.appendChild(button);
+     
+    // add these ths in tr 
+
+    tr.appendChild(idTd);
+    tr.appendChild(bookNameTd);
+    tr.appendChild(issueToTd);
+    tr.appendChild(issuedAtTd);
+    tr.appendChild(statusTd);
+
+    // add this tr in tbody
+    tableBody.appendChild(tr);
+}
+
+/**
+ * This will render all items present in the books into table
+ */
+function renderBooksInsideTable() {
+
+    // while(tableBody.firstChild) {
+    //     tableBody.removeChild(tableBody.firstChild)
+    // }
+
+    tableBody.innerHTML = "";
+
+   books.map(function (book, index) {
+
+    // This will create a row and add it inside tbody
+    createTableRow(book, tableBody,index+1);
+   })
+}
+
+function handleFormSubmit () {
+    // read book data 
+    const bookName = bookNameElement.value;
+    bookNameElement.value = "";
+
+    // read issued to data 
+    const issuedTo = issueToElement.value;
+    issueToElement.value = "";
+
+    // create a book object with issueAd and default status
+
+    if(bookName && issuedTo) {
+        const book = {
+          name: bookName,
+          issuedTo: issuedTo,
+          issuedAt: new Date().toUTCString().substring(0,25),
+          status: "not returned",
+        };
+
+        books.push(book);
+        renderBooksInsideTable();
+    }
+    else {
+        alert("You are trying to enter empty details");
+    }
+    
+}
+
+btnElement.addEventListener('click', handleFormSubmit);
